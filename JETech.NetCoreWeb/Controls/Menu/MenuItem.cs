ca123a8af21fcf94d19fdca3a6ce90af36cc721f
Jose Feliz <jfelizalmonte@gmail.com>
2020-10-28 14:28:30 -0400

    MenuITem

---
+++ JETech.NetCoreWeb/Controls/Menu/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JETech.NetCoreWeb.Controls.Menu
+{
+    public class MenuItem
+    {
+        public string text { get; set; }
+        public bool disabled { get; set; }
+        public string icon { get; set; }        
+        public IEnumerable<MenuItem> items { get; set; }
+    }
+}
