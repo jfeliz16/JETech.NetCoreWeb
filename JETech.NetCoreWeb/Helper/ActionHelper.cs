c4f10fd2f867980edf12251debcc011fc4c8a736
Jose Feliz <jfeliz@camsoft.com.do>
2020-11-23 17:24:33 -0500

    Update ActionHelper.cs

--- JETech.NetCoreWeb/Helper/ActionHelper.cs
+++ JETech.NetCoreWeb/Helper/ActionHelper.cs
@@ -10,10 +10,12 @@ namespace JETech.NetCoreWeb.Helper
         public ActionPaginationResult<IQueryable<t>> GetPaginationResult<t>(ActionQueryArgs<t> queryArgs,IQueryable<t> data)
         {
             IQueryable<t> newData;
+            int totalCount = 0;
+
             if (queryArgs.PageArgs != null)
             {
                 int skip = 0;
-                int take = 0;
+                int take = 0;                
 
                 if (queryArgs.PageArgs.Skip.HasValue)
                 {
@@ -21,14 +23,16 @@ namespace JETech.NetCoreWeb.Helper
                 }
                 if (queryArgs.PageArgs.Size.HasValue && queryArgs.PageArgs.Num.HasValue)
                 {
-                    take = (int)(queryArgs.PageArgs.Size * queryArgs.PageArgs.Num);
+                    totalCount = data.Count();
+                    take = (int)queryArgs.PageArgs.Size;
+                    skip = (int)(queryArgs.PageArgs.Size * queryArgs.PageArgs.Num);
                 }
                 newData = data.Skip(skip).Take(take);
             }else
             {
                 newData = data;
             }
-            return new ActionPaginationResult<IQueryable<t>> { Data = newData };
+            return new ActionPaginationResult<IQueryable<t>> { Data = newData, TotalCount = totalCount };
         } 
     }
 }
