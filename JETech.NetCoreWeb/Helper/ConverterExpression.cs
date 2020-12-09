54bc919a9eafb624b5aad170524c41e7fc814ed0
Jose Feliz <jfeliz@camsoft.com.do>
2020-12-09 14:37:28 -0500

    Create ConverterExpression.cs

---
+++ JETech.NetCoreWeb/Helper/ConverterExpression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace JETech.NetCoreWeb.Helper
+{
+    public class ConverterExpression
+    {
+        public static Expression<Func<T, bool>> GetContainsExpression<T>(string propertyName, string propertyValue)
+        {
+            var parameterExp = Expression.Parameter(typeof(T), "type");
+            var propertyExp = Expression.Property(parameterExp, propertyName);
+            MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+            var someValue = Expression.Constant(propertyValue, typeof(string));
+            var containsMethodExp = Expression.Call(propertyExp, method, someValue);
+
+            return Expression.Lambda<Func<T, bool>>(containsMethodExp, parameterExp);
+        }
+
+        public static Expression<Func<T, bool>> GetStartsWithExpression<T>(string propertyName, string propertyValue)
+        {
+            var parameterExp = Expression.Parameter(typeof(T), "type");
+            var propertyExp = Expression.Property(parameterExp, propertyName);
+            MethodInfo method = typeof(string).GetMethod("StartsWith", new[] { typeof(string) });
+            var someValue = Expression.Constant(propertyValue, typeof(string));
+            var containsMethodExp = Expression.Call(propertyExp, method, someValue);
+
+            return Expression.Lambda<Func<T, bool>>(containsMethodExp, parameterExp);
+        }
+    }
+}
