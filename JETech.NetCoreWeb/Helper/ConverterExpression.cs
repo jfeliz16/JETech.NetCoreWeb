0d2f8e070677643e674e1c3d12ec2ad261cefe68
Jose Feliz <jfeliz@camsoft.com.do>
2020-12-12 22:28:21 -0500

    Update ConverterExpression.cs

--- JETech.NetCoreWeb/Helper/ConverterExpression.cs
+++ JETech.NetCoreWeb/Helper/ConverterExpression.cs
@@ -8,13 +8,26 @@ namespace JETech.NetCoreWeb.Helper
 {
     public class ConverterExpression
     {
+        private static Expression<Func<T, bool>> GetExpressionFromMethod<T>(string propertyName, string propertyValue, string pMethod)
+        {
+            var parameterExp = Expression.Parameter(typeof(T), "type");
+            var propertyExp = Expression.Property(parameterExp, propertyName);
+            MethodInfo method = typeof(string).GetMethod(pMethod, new[] { typeof(string) });
+            var someValue = Expression.Constant(propertyValue, typeof(string));
+            var containsMethodExp = Expression.Call(propertyExp, method, someValue);
+
+            var condition = Expression.Lambda<Func<T, bool>>(containsMethodExp, parameterExp);
+
+            return condition;
+        }
+
         public static Expression<Func<T, bool>> GetContainsExpression<T>(string propertyName, string propertyValue) 
         {
             return GetContainsExpression<T>(propertyName, propertyValue, false);
         }
 
         public static Expression<Func<T, bool>> GetContainsExpression<T>(string propertyName, string propertyValue,bool validateNull)
-        {
+        {            
             var parameterExp = Expression.Parameter(typeof(T), "type");
             var propertyExp = Expression.Property(parameterExp, propertyName);
             MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
@@ -24,25 +37,37 @@ namespace JETech.NetCoreWeb.Helper
             var condition = Expression.Lambda<Func<T, bool>>(containsMethodExp, parameterExp);
 
             if (validateNull)
-            {
+            {             
                 var nullCheck2 = Expression.NotEqual(propertyExp, Expression.Constant(null, typeof(string)));
+                //var nullCheck2 = GetNotNullBinaryExpression<T>(propertyName,propertyValue);
+
+
+
+                //condition = Expression.Lambda<Func<T, bool>>(
+                //                Expression.AndAlso(
+                //                    condition.Body,
+                //                    Expression.Invoke(nullCheck2, condition.Parameters[0])), condition.Parameters[0]);
 
                 condition = Expression.Lambda<Func<T, bool>>(
-                               Expression.AndAlso(nullCheck2, condition.Body ), condition.Parameters[0]);
+                               Expression.AndAlso(nullCheck2, condition.Body), condition.Parameters[0]);
             }
-            
+
             return condition;
+        }
+
+        public static BinaryExpression GetNotNullBinaryExpression<T>(string propertyName, string propertyValue)
+        {
+            var parameterExp = Expression.Parameter(typeof(T), "type");
+            var propertyExp = Expression.Property(parameterExp, propertyName);
+            var condiction = Expression.NotEqual(propertyExp, Expression.Constant(null, typeof(string)));
+            
+
+            return condiction;
         }
 
         public static Expression<Func<T, bool>> GetStartsWithExpression<T>(string propertyName, string propertyValue)
         {
-            var parameterExp = Expression.Parameter(typeof(T), "type");
-            var propertyExp = Expression.Property(parameterExp, propertyName);
-            MethodInfo method = typeof(string).GetMethod("StartsWith", new[] { typeof(string) });
-            var someValue = Expression.Constant(propertyValue, typeof(string));
-            var containsMethodExp = Expression.Call(propertyExp, method, someValue);
-        
-            var condition = Expression.Lambda<Func<T, bool>>(containsMethodExp, parameterExp);
+            var condition = GetExpressionFromMethod<T>(propertyName, propertyValue, "StartsWith");
 
             return condition;
         }
