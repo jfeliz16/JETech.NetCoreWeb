8b95a4baabe7dd338804bd94517425ede198f88a
Jose Feliz <jfeliz@camsoft.com.do>
2021-02-01 21:35:43 -0500

    Add HttpPequestExtension

---
+++ JETech.NetCoreWeb/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JETech.NetCoreWeb.Extensions
+{
+    public static class HttpRequestExtension
+    {
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+            if (request.Headers != null)
+                return !string.IsNullOrEmpty(request.Headers["X-Requested-With"]) &&
+                    string.Equals(
+                        request.Headers["X-Requested-With"],
+                        "XmlHttpRequest",
+                        StringComparison.OrdinalIgnoreCase);
+
+            return false;
+        }
+    }
+}
