1ca8d1f44a3e5c1edf2657bf10b8e347eb14d833
Jose Feliz <jfeliz@camsoft.com.do>
2021-02-03 18:16:16 -0500

    Traiying to render script into partial view

---
+++ JETech.NetCoreWeb/Attributes/RenderAjaxPartialScriptsResponseFilter.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using JETech.NetCoreWeb.Extensions;
+
+namespace JETech.NetCoreWeb.Attributes
+{
+    public class RenderAjaxPartialScriptsResponseFilter : MemoryStream
+    {
+        private readonly Stream _response;
+        private readonly ActionExecutingContext _filterContext;
+
+        public RenderAjaxPartialScriptsResponseFilter(Stream response, ActionExecutingContext filterContext)
+        {
+            _response = response;
+            _filterContext = filterContext;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _response.WriteAsync(buffer, offset, count);
+        }
+
+        public override void Flush()
+        {
+            var scriptsHtml = GetScripts();
+            var buffer = Encoding.UTF8.GetBytes(scriptsHtml);
+            _response.WriteAsync(buffer, 0, buffer.Length);
+            base.Flush();
+        }
+
+        private string GetScripts()
+        {
+            string html = "";
+            var itemsToRemove = new List<object>();
+            foreach (object key in _filterContext.HttpContext.Items.Keys)
+            {
+                if (key.ToString().StartsWith("_script_"))
+                {
+                    var template = _filterContext.HttpContext.Items[key] as Func<object, HelperResult>;
+                    if (template != null)
+                    {
+                        html += (template(null));
+                        itemsToRemove.Add(key);
+                    }
+                }
+            }
+            foreach (var key in itemsToRemove)
+                _filterContext.HttpContext.Items.Remove(key);
+
+            return html;
+        }
+    }
+
+    /// <summary>
+    /// Appends partial view scripts to the html response of an AJAX request
+    /// </summary>
+    public class RenderAjaxPartialScriptsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                var response = filterContext.HttpContext.Response;                
+                if (response.Body != null)
+                    response.Body = new RenderAjaxPartialScriptsResponseFilter(response.Body, filterContext);
+            }
+        }
+    }
+}
---
+++ JETech.NetCoreWeb/Extensions/HtmlExtension.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+
+namespace JETech.NetCoreWeb.Extensions
+{
+    public static class HtmlExtensions
+    {
+        private const string ScriptsKey = "DelayedScripts";
+
+        public static IDisposable BeginScripts(this IHtmlHelper helper)
+        {
+            return new ScriptBlock(helper.ViewContext);
+        }
+
+        public static HtmlString PageScripts(this IHtmlHelper helper)
+        {
+            return new HtmlString(string.Join(Environment.NewLine, GetPageScriptsList(helper.ViewContext.HttpContext)));
+        }
+
+        private static List<string> GetPageScriptsList(Microsoft.AspNetCore.Http.HttpContext httpContext)
+        {
+            var pageScripts = (List<string>)httpContext.Items[ScriptsKey];
+            if (pageScripts == null)
+            {
+                pageScripts = new List<string>();
+                httpContext.Items[ScriptsKey] = pageScripts;
+            }
+            return pageScripts;
+        }
+
+        private class ScriptBlock : IDisposable
+        {
+            private readonly TextWriter _originalWriter;
+            private readonly StringWriter _scriptsWriter;
+
+            private readonly ViewContext _viewContext;
+
+            public ScriptBlock(ViewContext viewContext)
+            {
+                _viewContext = viewContext;
+                _originalWriter = _viewContext.Writer;
+                _viewContext.Writer = _scriptsWriter = new StringWriter();
+            }
+
+            public void Dispose()
+            {
+                _viewContext.Writer = _originalWriter;
+                var pageScripts = GetPageScriptsList(_viewContext.HttpContext);
+                pageScripts.Add(_scriptsWriter.ToString());
+            }
+        }
+
+
+        public static HtmlString Script(this IHtmlHelper htmlHelper, Func<object, HelperResult> template)
+        {
+            htmlHelper.ViewContext.HttpContext.Items["_script_" + Guid.NewGuid()] = template;
+            return HtmlString.Empty;
+        }
+
+        public static HtmlString RenderScripts(this IHtmlHelper htmlHelper)
+        {
+            foreach (object key in htmlHelper.ViewContext.HttpContext.Items.Keys)
+            {
+                if (key.ToString().StartsWith("_script_"))
+                {
+                    var template = htmlHelper.ViewContext.HttpContext.Items[key] as Func<object, HelperResult>;
+                    if (template != null)
+                    {
+                        htmlHelper.ViewContext.Writer.Write(template(null));
+                    }
+                }
+            }
+            return HtmlString.Empty;
+        }
+    }
+}
